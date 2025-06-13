4829e0d3adee08d88a02f3e49c2f5584ae8ebe5b
sayaliambure <sayaliambure15@gmail.com>
2025-06-13 13:32:07 +0530

    vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//         rewrite: path => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// }); 
+
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()]
 });
